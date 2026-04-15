7a8c50db266ced4e9160cec290c8b0b3dd1db2da
Daniel Gomez Jimenez <archdanblanchin@gmail.com>
2026-04-15 13:06:05 +0000

    File changes

--- src/pages/CantiereDetail.jsx
+++ src/pages/CantiereDetail.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useState, useEffect } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
